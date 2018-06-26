596db65746ca9fe07e37c800b477418bda759d3d
Joel Rennich <joel@orchardandgrove.com>
2018-06-26 11:14:21 -0500

    add os.log support and pulling from a defaults setting to engage verbose mode

--- NoMAD-ADAuth/Logger.swift
+++ NoMAD-ADAuth/Logger.swift
@@ -11,6 +11,7 @@
 let myLogger = Logger()
 
 import Foundation
+import os.log
 
 /// The individual logging levels to use when logging in NoMAD
 ///
@@ -19,65 +20,60 @@ import Foundation
 /// - notice: Nice to know issues that may, or may not, cause issues
 /// - debug: Lots of verbose logging
 enum LogLevel: Int {
-
+    
     /// General errors
     case base = 0
-
+    
     /// Positive info
     case info = 1
-
+    
     /// Nice to know issues that may, or may not, cause issues
     case notice = 2
-
+    
     /// Lots of verbose logging
     case debug = 3
 }
 
+var log: OSLog? {
+    if #available(OSX 10.12, *) {
+        return OSLog(subsystem: "menu.nomad.login.ad", category: "framework")
+    } else {
+        return nil
+    }
+}
+
+
 
 /// Simple class to handle logging levels. Use the `LogLevel` enum to specify the logging details.
 class Logger {
-
+    
     /// Set to a level from `LogLevel` enum to control what gets logged.
     var loglevel: LogLevel
-
+    
     /// Init method simply check to see if Verbose logging is enabled or not for the Logger object.
     init() {
-        /*
-        if (defaults.bool(forKey: "Verbose") == true) {
+        
+        let defaults = UserDefaults.init(suiteName: "menu.nomad.login.ad")
+        
+        if defaults?.bool(forKey: "Verbose") ?? false {
             loglevel = .debug
-            logit(.debug, message: "Debug logging enabled")
-        } else if (CommandLine.arguments.contains("-v")) {
-            loglevel = .debug
-            logit(.debug, message: "Debug logging enabled via flag")
         } else {
             loglevel = .base
         }
-         */
-        loglevel = .base
     }
-
+    
     /// Simple wrapper around NSLog to provide control of logging.
     ///
     /// - Parameters:
     ///   - level: A value from `LogLevel` enum
     ///   - message: A `String` that describes the information to be logged
     func logit(_ level: LogLevel, message: String) {
-        if (level.rawValue <= loglevel.rawValue) && !CommandLine.arguments.contains("-v") {
-            
-            // sanitize the message
-            
-            var set = CharacterSet.alphanumerics
-            
-            set.formUnion(CharacterSet.whitespaces)
-            set.formUnion(CharacterSet.decimalDigits)
-            set.formUnion(CharacterSet.init(charactersIn: "-:.,$@[]"))
-            
-            // anything not in the set, percent encode for safety
-            
-            guard let logMessage = message.addingPercentEncoding(withAllowedCharacters: set) else { return }
-            NSLog("level: \(level) - " + logMessage)
-        } else {
-            NSLog("level: \(level) - " + message)
+        if (level.rawValue <= loglevel.rawValue) {
+            if #available(OSX 10.12, *) {
+                os_log("%{public}@", log: log!, type: .debug)
+            } else {
+                NSLog("level: \(level) - " + message)
+            }
         }
     }
 }
