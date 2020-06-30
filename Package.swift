d4a33230fbc3333fb4ef6b24e13cf7b54851b01d
Joel Rennich <joel@orchardandgrove.com>
2020-06-29 20:36:33 -0500

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3.
+//
+//  Package.swift
+//  NoMAD-ADAuth
+//
+//  Created by Joel Rennich on 6/29/20.
+//  Copyright © 2020 Orchard & Grove Inc. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "NoMAD-ADAuth",
+  platforms: [
+    .macOS(.v10_12),
+  ],
+  products: [
+    .library(
+      name: "NoMAD_ADAuth",
+      targets: ["NoMAD_ADAuth"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "NoMAD_ADAuth"
+    )
+  ]
+)
